61126d6335e2940edff62d0950d1aadf4d057f61
Abel Svoboda <abel465@gmail.com>
2025-05-18 11:10:23 +1200

    Seperate ui function into parts

--- runner/src/controller/ui.rs
+++ runner/src/controller/ui.rs
@@ -10,27 +10,98 @@ impl Controller {
         ui_state: &UiState,
         graphics_context: &easy_shader_runner::GraphicsContext,
     ) {
-        let width = 120.0;
         if let Some(pos) = self.context_menu {
-            let r = egui::Window::new("right_click_menu")
-                .frame(egui::Frame::NONE)
-                .title_bar(false)
-                .resizable(false)
-                .fixed_pos(egui::pos2(pos.x, pos.y))
-                .show(ctx, |ui| {
-                    if ui.button("Show iterations here").clicked() {
-                        self.iterations.marker = self.to_uv(pos);
-                        self.iterations.enabled = true;
-                        self.iterations.recompute = true;
-                        self.context_menu = None;
-                    }
-                });
-            if let Some(r) = r {
-                if r.response.clicked_elsewhere() {
-                    self.context_menu = None;
+            self.context_menu_window(ctx, pos);
+        }
+        self.main_window(ctx);
+        self.handle_cursor_icon(ctx);
+
+        if self.iterations.recompute {
+            self.recompute_iterations(graphics_context);
+        }
+        if self.show_fps {
+            self.fps_window(ctx, ui_state);
+        }
+        if self.debug {
+            self.debug_window(ctx);
+        }
+    }
+
+    fn recompute_iterations(&mut self, graphics_context: &easy_shader_runner::GraphicsContext) {
+        use shared::complex::Complex;
+        self.iterations.points.clear();
+        let c = Complex::from(self.iterations.marker);
+        let mut z = Complex::ZERO;
+        let mut n2_container = None;
+        for _ in 0..self.num_iterations as u32 {
+            z = z * z + c;
+            let norm_squared = z.norm_squared();
+            if norm_squared >= 4.0 {
+                if n2_container.is_none() {
+                    n2_container = Some(norm_squared);
                 }
             }
+            if self.iterations.points.last().is_some_and(|p| p == &z.0)
+                || z.x.abs() > 1000.0
+                || z.y.abs() > 1000.0
+            {
+                break;
+            }
+            self.iterations.points.push(z.0);
         }
+        if let Some(n2) = n2_container {
+            self.iterations.norm_squared_value = n2;
+        } else {
+            self.iterations.norm_squared_value = 0.0;
+        }
+
+        if self.iterations.enabled && self.iterations.points.len() > 0 {
+            graphics_context.queue.write_buffer(
+                self.iterations.points_buffer.as_ref().unwrap(),
+                0,
+                bytemuck::cast_slice(&self.iterations.points),
+            );
+        }
+        self.iterations.recompute = false;
+    }
+
+    fn handle_cursor_icon(&mut self, ctx: &egui::Context) {
+        if self.iterations.dragging {
+            ctx.set_cursor_icon(egui::CursorIcon::Grabbing);
+        } else if let Some(icon) = self.render_split.dragging {
+            ctx.set_cursor_icon(icon);
+        } else if self.can_grab_marker() {
+            ctx.set_cursor_icon(egui::CursorIcon::Grab);
+        } else if let Some(icon) = self.can_grab_render_split() {
+            ctx.set_cursor_icon(icon);
+        } else {
+            ctx.set_cursor_icon(egui::CursorIcon::Default);
+        }
+    }
+
+    fn context_menu_window(&mut self, ctx: &egui::Context, pos: Vec2) {
+        let r = egui::Window::new("right_click_menu")
+            .frame(egui::Frame::NONE)
+            .title_bar(false)
+            .resizable(false)
+            .fixed_pos(egui::pos2(pos.x, pos.y))
+            .show(ctx, |ui| {
+                if ui.button("Show iterations here").clicked() {
+                    self.iterations.marker = self.to_uv(pos);
+                    self.iterations.enabled = true;
+                    self.iterations.recompute = true;
+                    self.context_menu = None;
+                }
+            });
+        if let Some(r) = r {
+            if r.response.clicked_elsewhere() {
+                self.context_menu = None;
+            }
+        }
+    }
+
+    fn main_window(&mut self, ctx: &egui::Context) {
+        let width = 120.0;
         egui::Window::new("ui")
             .min_width(width)
             .max_width(width)
@@ -89,70 +160,20 @@ impl Controller {
                     ui.checkbox(&mut self.show_fps, "Show FPS");
                 });
             });
-        if self.iterations.dragging {
-            ctx.set_cursor_icon(egui::CursorIcon::Grabbing);
-        } else if let Some(icon) = self.render_split.dragging {
-            ctx.set_cursor_icon(icon);
-        } else if self.can_grab_marker() {
-            ctx.set_cursor_icon(egui::CursorIcon::Grab);
-        } else if let Some(icon) = self.can_grab_render_split() {
-            ctx.set_cursor_icon(icon);
-        } else {
-            ctx.set_cursor_icon(egui::CursorIcon::Default);
-        }
-        if self.iterations.recompute {
-            use shared::complex::Complex;
-            self.iterations.points.clear();
-            let c = Complex::from(self.iterations.marker);
-            let mut z = Complex::ZERO;
-            let mut n2_container = None;
-            for _ in 0..self.num_iterations as u32 {
-                z = z * z + c;
-                let norm_squared = z.norm_squared();
-                if norm_squared >= 4.0 {
-                    if n2_container.is_none() {
-                        n2_container = Some(norm_squared);
-                    }
-                }
-                if self.iterations.points.last().is_some_and(|p| p == &z.0)
-                    || z.x.abs() > 1000.0
-                    || z.y.abs() > 1000.0
-                {
-                    break;
-                }
-                self.iterations.points.push(z.0);
-            }
-            if let Some(n2) = n2_container {
-                self.iterations.norm_squared_value = n2;
-            } else {
-                self.iterations.norm_squared_value = 0.0;
-            }
-
-            if self.iterations.enabled && self.iterations.points.len() > 0 {
-                graphics_context.queue.write_buffer(
-                    self.iterations.points_buffer.as_ref().unwrap(),
-                    0,
-                    bytemuck::cast_slice(&self.iterations.points),
-                );
-            }
-            self.iterations.recompute = false;
-        }
-        if self.show_fps {
-            egui::Window::new("fps")
-                .title_bar(false)
-                .resizable(false)
-                .interactable(false)
-                .anchor(egui::Align2::RIGHT_BOTTOM, egui::Vec2::splat(-10.0))
-                .show(ctx, |ui| {
-                    ui.label(format!("FPS: {}", ui_state.fps));
-                });
-        }
-        if self.debug {
-            self.debug_ui(ctx);
-        }
     }
 
-    pub fn debug_ui(&mut self, ctx: &egui::Context) {
+    fn fps_window(&mut self, ctx: &egui::Context, ui_state: &UiState) {
+        egui::Window::new("fps")
+            .title_bar(false)
+            .resizable(false)
+            .interactable(false)
+            .anchor(egui::Align2::RIGHT_BOTTOM, egui::Vec2::splat(-10.0))
+            .show(ctx, |ui| {
+                ui.label(format!("FPS: {}", ui_state.fps));
+            });
+    }
+
+    fn debug_window(&mut self, ctx: &egui::Context) {
         egui::Window::new("debug_window")
             .title_bar(false)
             .resizable(false)
